d3a9001c2889f26e8ae4d5a8055db9c96c4d7264
Adam Kenneweg <akennewe@uwaterloo.ca>
2014-09-20 07:38:13 -0400

    First screen

--- src/ca/waterloo/watisfood/MainActivity.java
+++ src/ca/waterloo/watisfood/MainActivity.java
@@ -2,6 +2,12 @@ package ca.waterloo.watisfood;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends Activity {
     /**
@@ -9,7 +15,27 @@ public class MainActivity extends Activity {
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button buttonOpen = (Button)findViewById(R.id.buttonOpen);
+        buttonOpen.setOnClickListener(openListener); // Register the onClick listener with the implementation above
+
+        Button buttonAll = (Button)findViewById(R.id.buttonAll);
+        buttonAll.setOnClickListener(allListener); // Register the onClick listener with the implementation above
     }
+
+    //Create an anonymous implementation of OnClickListener
+    private OnClickListener openListener = new OnClickListener() {
+        public void onClick(View v) {
+          Toast.makeText(MainActivity.this, "The Start button was clicked.", Toast.LENGTH_LONG).show();
+        }
+    };
+
+    // Create an anonymous implementation of OnClickListener
+    private OnClickListener allListener = new OnClickListener() {
+        public void onClick(View v) {
+         Toast.makeText(MainActivity.this, "The Stop button was clicked.", Toast.LENGTH_LONG).show();
+        }
+    };
 }
